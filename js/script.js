4d5808900a4d6e06057c309a72ce515ed8f706f1
bmajors23 <bmajors23@gmail.com>
2022-07-10 13:08:06 -0400

    Add Tools UI & Functionality

--- js/script.js
+++ js/script.js
@@ -1,14 +1,50 @@
 const container = document.querySelector(".container");
 let box;
-let gridSize = 16;
+let slider = document.getElementById("sliderID");
+let gridSize = slider.value;
+const clearBtn = document.querySelector(".clearBtn");
+clearBtn.addEventListener("click", () => {
+    clear();
+})
+createGrid();
 
-for (let i = 0; i < gridSize; i++) {
-    box = document.createElement("div");
-    box.classList.add("columns");
-    container.appendChild(box);
-    for (let j = 0; j < gridSize; j++) {
-        innerBox = document.createElement("div");
-        innerBox.classList.add("boxes");
-        box.appendChild(innerBox);
-        }
-    }
+
+function createGrid() {
+    for (let i = 0; i < gridSize; i++) {
+        box = document.createElement("div");
+        box.classList.add("columns");
+        container.appendChild(box);
+        for (let j = 0; j < gridSize; j++) {
+            innerBox = document.createElement("div");
+            innerBox.classList.add("boxes");
+            box.appendChild(innerBox);
+            };
+        };
+        const boxes = document.querySelectorAll(".boxes");
+        boxes.forEach(box => {
+            box.addEventListener("mouseover", () => {
+                box.setAttribute("style", "background-color: black");
+            });
+        });
+};
+  
+slider.oninput = function() {
+  gridSize = this.value;
+  destroyGrid();
+  createGrid();
+}
+
+const columns = document.querySelectorAll(".columns");
+
+function destroyGrid() {
+while (container.firstChild) {
+    container.removeChild(container.firstChild);
+};
+};
+
+function clear() {
+    const boxes = document.querySelectorAll(".boxes");
+    boxes.forEach(box => {
+        box.setAttribute("style", "background-color: black");
+    });
+};
